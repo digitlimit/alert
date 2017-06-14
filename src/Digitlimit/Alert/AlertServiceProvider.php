59363c52b0bbe6baf6e1c5a49a89bab7880f7082
Emeka Mbah <frankemeks77@yahoo.com>
2017-06-14 10:22:32 +0100

    Added Directories

---
+++ src/Digitlimit/Alert/AlertServiceProvider.php
@@ -0,0 +1,39 @@
+<?php namespace Digitlimit\Alert;
+
+use Illuminate\Support\ServiceProvider;
+
+class AlertServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('digitlimit.alert', function ($app) {
+            return new Alert($app['session.store']);
+        });
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom( __DIR__ . '/views', 'alert');
+        $this->publishes([
+            __DIR__ . '/views' => base_path('resources/views/vendor/alert')
+        ]);
+    }
+}
