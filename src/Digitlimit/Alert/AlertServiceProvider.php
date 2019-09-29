d47635df29de39a20c0b2b039ad5a9058a7f1654
Mbah Emeka <frankemeks77@yahoo.com>
2019-09-29 10:46:09 +0100

    Added helper blade directives

--- src/Digitlimit/Alert/AlertServiceProvider.php
+++ src/Digitlimit/Alert/AlertServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Digitlimit\Alert;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AlertServiceProvider extends ServiceProvider
 {
@@ -36,5 +37,39 @@ class AlertServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/vendor/alert'),
         ]);
+
+        
+        Blade::directive('alertHasSuccess', function() {
+            return "<?php if(Alert::hasSuccess()): ?>";
+        });
+
+        Blade::directive('endAlertHasSuccess', function() {
+            return "<?php endif; ?>";
+        });
+
+        Blade::directive('alertHasNoSuccess', function() {
+            return "<?php if(!Alert::hasSuccess()): ?>";
+        });
+
+        Blade::directive('endAlertHasNoSuccess', function() {
+            return "<?php endif; ?>";
+        });
+
+
+        Blade::directive('alertHasError', function() {
+            return "<?php if(Alert::hasError()): ?>";
+        });
+
+        Blade::directive('endAlertHasError', function() {
+            return "<?php endif; ?>";
+        });
+
+        Blade::directive('alertHasNoError', function() {
+            return "<?php if(!Alert::hasError()): ?>";
+        });
+
+        Blade::directive('endAlertHasNoError', function() {
+            return "<?php endif; ?>";
+        });
     }
 }
