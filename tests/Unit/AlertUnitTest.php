7aa67defa69a20e7088b9457434e261f22020f0d
Emeka Mbah <frankemeks77@yahoo.com>
2023-04-30 02:05:26 +0100

    fix tests

--- tests/Unit/AlertUnitTest.php
+++ tests/Unit/AlertUnitTest.php
@@ -2,17 +2,56 @@
 use Digitlimit\Alert\Message;
 use Digitlimit\Alert\Enums\LevelType;
 
-it('can alert', function () 
+it('has alert with given message', function () 
 {
     $alert = alert();
-    $alert
-        ->tag('page footer')
-        ->info()
-        ->message('Thank you!');
+    $alert->message('Thank you!');
 
     $alerter = $alert->alerter();
-    
-    expect($alerter->getKey())->toBe('digitlimit.alert.page-footer');
-    expect($alerter->getLevel()->type())->toEqual(LevelType::INFO);
     expect($alerter->getMessage())->toEqual(new Message('Thank you!'));
+});
+
+it('has alert with given tag', function () 
+{
+    $alert = alert();
+    $alert->tag('page footer');
+
+    $alerter = $alert->alerter();
+    expect($alerter->getKey())->toBe('digitlimit.alert.page-footer');
+});
+
+it('has alert with info level', function () 
+{
+    $alert = alert();
+    $alert->info();
+
+    $alerter = $alert->alerter();
+    expect($alerter->getLevel()->type())->toEqual(LevelType::INFO);
+});
+
+it('has alert with success level', function () 
+{
+    $alert = alert();
+    $alert->success();
+
+    $alerter = $alert->alerter();
+    expect($alerter->getLevel()->type())->toEqual(LevelType::SUCCESS);
+});
+
+it('has alert with error level', function () 
+{
+    $alert = alert();
+    $alert->error();
+
+    $alerter = $alert->alerter();
+    expect($alerter->getLevel()->type())->toEqual(LevelType::ERROR);
+});
+
+it('has alert with warning level', function () 
+{
+    $alert = alert();
+    $alert->warning();
+
+    $alerter = $alert->alerter();
+    expect($alerter->getLevel()->type())->toEqual(LevelType::WARNING);
 });
