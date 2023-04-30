5b2b28e0dd487c9daa60129ab367451f7a76ac7f
Emeka Mbah <frankemeks77@yahoo.com>
2023-04-30 21:18:25 +0100

    update view

---
+++ resources/views/components/notify.blade.php
@@ -0,0 +1,11 @@
+<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="..." class="rounded me-2" alt="...">
+      <strong class="me-auto">Bootstrap</strong>
+      <small>11 mins ago</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      Hello, world! This is a toast message.
+    </div>
+</div>
--- src/AlertServiceProvider.php
+++ src/AlertServiceProvider.php
@@ -2,23 +2,24 @@
 namespace Digitlimit\Alert;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+use Digitlimit\View\Components;
 
 class AlertServiceProvider extends ServiceProvider
 {
     /**
      * Perform post-registration booting of services.
-     *
-     * @return void
      */
     public function boot(): void
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'alert');
+
+        $this->registerComponents();
     }
 
     /**
      * Register any package services.
-     *
-     * @return void
      */
     public function register(): void
     {
@@ -30,23 +31,33 @@ class AlertServiceProvider extends ServiceProvider
 
     /**
      * Get the services provided by the provider.
-     *
-     * @return array
      */
-    public function provides()
+    public function provides() : array
     {
         return ['alert'];
     }
 
     /**
      * Console-specific booting.
-     *
-     * @return void
      */
     protected function bootForConsole(): void
     {
         $this->publishes([
             __DIR__.'/../resources/views' => base_path('resources/views/vendor/digitlimit'),
         ], 'alert.views');
+    }
+
+    /**
+     * Register alert components
+     */
+    protected function registerComponents() : void
+    {
+        Blade::componentNamespace('Digitlimit\\Views\\Components', 'alert');
+
+        Blade::component('alert-bar',    Components\Bar::class);
+        Blade::component('alert-field',  Components\Field::class);
+        Blade::component('alert-modal',  Components\Modal::class);
+        Blade::component('alert-notify', Components\Notify::class);
+        Blade::component('alert-sticky', Components\Sticky::class);
     }
 }
