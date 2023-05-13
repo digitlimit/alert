2e5a1d9dec542c332d9c6f1146967ec3466d8e79
Emeka Mbah <frankemeks77@yahoo.com>
2023-05-14 00:37:16 +0100

    add tests

--- tests/Feature/Components/FieldTest.php
+++ tests/Feature/Components/FieldTest.php
@@ -15,7 +15,7 @@ it('can render a default field view component', function ()
         ->assertSee('class="form-text text-success"', false)
         ->assertSee('Username is available');
   
-})->name('view-component', 'view-component-field');
+})->name('view-component', 'view-component-field-default');
 
 it('can render a tagged field view component', function () 
 {
@@ -31,4 +31,28 @@ it('can render a tagged field view component', function ()
         ->assertSee('class="form-text text-warning"', false)
         ->assertSee('Some errors occurred!');
   
-})->name('view-component', 'view-component-field');
+})->name('view-component', 'view-component-field-tagged');
+
+it('can render a named field view component', function () 
+{
+    Alert::field('Good, you chose a valid country')
+        ->tag('contact')
+        ->name('country')
+        ->success()
+        ->flash();
+
+    // Alert::field('Will not see this message because it has different name')
+    //     ->tag('contact')
+    //     ->name('state')
+    //     ->success()
+    //     ->flash();
+
+    $view = $this
+        ->blade('<x-alert-field name="country" tag="contact" />');
+
+    $view
+        ->assertSee('class="form-text text-success"', false)
+        ->assertSee('Good, you chose a valid country');
+  
+})->name('view-component', 'view-component-field-tagged');
+
