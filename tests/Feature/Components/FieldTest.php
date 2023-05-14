1d9e6286b2a2015c7e7c23530eedc13aa5318397
Emeka Mbah <frankemeks77@yahoo.com>
2023-05-14 17:16:39 +0100

    add field validation errors tests

--- tests/Feature/Components/FieldTest.php
+++ tests/Feature/Components/FieldTest.php
@@ -59,3 +59,30 @@ it('can render a named field view component', function ()
   
 })->name('view-component', 'view-component-field-tagged');
 
+it('can render validation errors for the field view component', function () 
+{
+    $validator = validator(
+        ['firstname' => '', 'lastname' => ''], 
+        [
+            'firstname' => 'required', 
+            'lastname'  => 'required'
+        ]
+    );
+
+    Alert::field()
+        ->tag('contact')
+        ->errors($validator)
+        ->error()
+        ->flash();
+
+    $this
+        ->blade('<x-alert-field name="firstname" tag="contact" />')
+        ->assertSee('class="form-text text-danger"', false)
+        ->assertSee('The firstname field is required');
+
+    $this
+        ->blade('<x-alert-field name="lastname" tag="contact" />')
+        ->assertSee('class="form-text text-danger"', false)
+        ->assertSee('The lastname field is required');
+  
+})->name('view-component', 'view-component-field-tagged');
