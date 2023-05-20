0eefe98608cd3ee0aa1775199a1b6d6d6c9e8899
Emeka Mbah <frankemeks77@yahoo.com>
2023-05-20 18:31:05 +0100

    created more test cases

--- tests/Feature/Types/NotifyTest.php
+++ tests/Feature/Types/NotifyTest.php
@@ -4,7 +4,8 @@ use Digitlimit\Alert\Facades\Alert;
 use Digitlimit\Alert\Message\MessageInterface;
 use Digitlimit\Alert\Types\Notify;
 
-it('can create a notify alert', function () {
+it('can create a notify alert', function () 
+{
     Alert::notify('Thank you!')->flash();
 
     $default = Alert::default('notify');
@@ -12,4 +13,57 @@ it('can create a notify alert', function () {
     expect($default)->toBeInstanceOf(MessageInterface::class);
     expect($default)->toBeInstanceOf(Notify::class);
     expect($default->message)->toEqual('Thank you!');
+    expect($default->key())->toEqual('notify');
 })->name('types', 'types-notify', 'types-notify');
+
+it('can create a notify alert position', function () 
+{
+    // top right
+    Alert::notify('Thank you!')
+    ->topRight()
+    ->flash();
+
+    $default = Alert::default('notify');
+    expect($default->position)->toEqual('top-0 end-0');
+
+    // top left
+    Alert::notify('Thank you!')
+    ->topLeft()
+    ->flash();
+
+    $default = Alert::default('notify');
+    expect($default->position)->toEqual('top-0 start-0');
+
+    // bottom right
+    Alert::notify('Thank you!')
+    ->bottomRight()
+    ->flash();
+
+    $default = Alert::default('notify');
+    expect($default->position)->toEqual('bottom-0 end-0');
+
+    // bottom left
+    Alert::notify('Thank you!')
+    ->bottomLeft()
+    ->flash();
+
+    $default = Alert::default('notify');
+    expect($default->position)->toEqual('bottom-0 start-0');
+
+    // centered
+    Alert::notify('Thank you!')
+    ->centered()
+    ->flash();
+
+    $default = Alert::default('notify');
+    expect($default->position)->toEqual('top-50 start-50 translate-middle');
+
+    // bottom center
+    Alert::notify('Thank you!')
+    ->bottomCenter()
+    ->flash();
+
+    $default = Alert::default('notify');
+    expect($default->position)->toEqual('bottom-0 start-50 translate-middle-x');
+
+})->name('types', 'types-notify', 'types-notify-position');
--- tests/Feature/Types/StickyTest.php
+++ tests/Feature/Types/StickyTest.php
@@ -3,6 +3,7 @@
 use Digitlimit\Alert\Facades\Alert;
 use Digitlimit\Alert\Message\MessageInterface;
 use Digitlimit\Alert\Types\Sticky;
+use Digitlimit\Alert\Component\Button;
 
 it('can create a sticky alert', function () {
     Alert::sticky('Thank you!')->flash();
@@ -12,4 +13,33 @@ it('can create a sticky alert', function () {
     expect($default)->toBeInstanceOf(MessageInterface::class);
     expect($default)->toBeInstanceOf(Sticky::class);
     expect($default->message)->toEqual('Thank you!');
-})->name('types', 'types-sticky', 'types-sticky');
+})->name('types', 'types-sticky', 'types-sticky-message');
+
+it('can create a sticky alert title', function () 
+{
+    Alert::sticky('Thank for ordering pizza!')
+    ->title('Order')
+    ->flash();
+
+    $default = Alert::default('sticky');
+
+    expect($default)->toBeInstanceOf(MessageInterface::class);
+    expect($default)->toBeInstanceOf(Sticky::class);
+    expect($default->title)->toEqual('Order');
+    expect($default->message)->toEqual('Thank for ordering pizza!');
+
+})->name('types', 'types-sticky', 'types-sticky-title');
+
+it('can create a sticky alert button', function () 
+{
+    Alert::sticky()
+    ->action('Yes')
+    ->flash();
+
+    $default = Alert::default('sticky');
+
+    expect($default)->toBeInstanceOf(MessageInterface::class);
+    expect($default)->toBeInstanceOf(Sticky::class);
+    expect($default->action)->toBeInstanceOf(Button::class);
+
+})->name('types', 'types-sticky', 'types-sticky-button');
