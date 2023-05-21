62ec7977f7c16ed6c0a9085b757a880f4411b1bf
Emeka Mbah <digitlimit@users.noreply.github.com>
2023-05-21 08:13:43 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -4,8 +4,7 @@ use Digitlimit\Alert\Facades\Alert;
 use Digitlimit\Alert\Message\MessageInterface;
 use Digitlimit\Alert\Types\Message;
 
-it('can create alert from helper function', function () 
-{
+it('can create alert from helper function', function () {
     // $alert = alert();
     // expect($alert)->toBeInstanceOf(Alert::class);
 
@@ -13,5 +12,4 @@ it('can create alert from helper function', function ()
     // expect($default)->toBeInstanceOf(MessageInterface::class);
     // expect($default)->toBeInstanceOf(Message::class);
     // expect($default->message)->toEqual('Thank you!');
-
 })->name('helpers', 'helpers-functions');
