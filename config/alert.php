d776bb5121ddf966d67c153293990a5e5996b980
Emeka Mbah <frankemeks77@yahoo.com>
2024-10-01 08:43:57 +0100

    refactor config to use themes

--- config/alert.php
+++ config/alert.php
@@ -1,53 +1,9 @@
 <?php
 
+use Digitlimit\Alert\View\Components\Themes;
+use Digitlimit\Alert\Types;
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Alert Types
-    |--------------------------------------------------------------------------
-    |
-    | Here you may register your custom alert types
-    |
-    */
-
-    'types' => [
-        'message' => [
-            'view'      => 'alert-message',
-            'alert'     => \Digitlimit\Alert\Types\Message::class,
-            'component' => \Digitlimit\Alert\View\Components\Themes\Bootstrap5\Message::class,
-        ],
-
-        'field' => [
-            'view'      => 'alert-field',
-            'alert'     => \Digitlimit\Alert\Types\Field::class,
-            'component' => \Digitlimit\Alert\View\Components\Themes\Bootstrap5\Field::class,
-        ],
-
-        'bag' => [
-            'view'      => 'alert-field',
-            'alert'     => \Digitlimit\Alert\Types\FieldBag::class,
-            'component' => \Digitlimit\Alert\View\Components\Themes\Bootstrap5\Field::class,
-        ],
-
-        'modal' => [
-            'view'      => 'alert-modal',
-            'alert'     => \Digitlimit\Alert\Types\Modal::class,
-            'component' => \Digitlimit\Alert\View\Components\Themes\Bootstrap5\Modal::class,
-        ],
-
-        'notify' => [
-            'view'      => 'alert-notify',
-            'alert'     => \Digitlimit\Alert\Types\Notify::class,
-            'component' => \Digitlimit\Alert\View\Components\Themes\Bootstrap5\Notify::class,
-        ],
-
-        'sticky' => [
-            'view'      => 'alert-sticky',
-            'alert'     => \Digitlimit\Alert\Types\Sticky::class,
-            'component' => \Digitlimit\Alert\View\Components\Themes\Bootstrap5\Sticky::class,
-        ],
-    ],
 
     /*
      |--------------------------------------------------------------------------
@@ -72,6 +28,43 @@ return [
         'bootstrap5' => [
             'views'      => 'components/themes/bootstrap5',
             'components' => 'Digitlimit\Alert\View\Components\Themes\Bootstrap5',
+            'types' => [
+                'message' => [
+                    'view'      => 'alert-message',
+                    'alert'     => Types\Message::class,
+                    'component' => Themes\Bootstrap5\Message::class,
+                ],
+
+                'field' => [
+                    'view'      => 'alert-field',
+                    'alert'     => Types\Field::class,
+                    'component' => Themes\Bootstrap5\Field::class,
+                ],
+
+                'bag' => [
+                    'view'      => 'alert-field',
+                    'alert'     => Types\FieldBag::class,
+                    'component' => Themes\Bootstrap5\Field::class,
+                ],
+
+                'modal' => [
+                    'view'      => 'alert-modal',
+                    'alert'     => Types\Modal::class,
+                    'component' => Themes\Bootstrap5\Modal::class,
+                ],
+
+                'notify' => [
+                    'view'      => 'alert-notify',
+                    'alert'     => Types\Notify::class,
+                    'component' => Themes\Bootstrap5\Notify::class,
+                ],
+
+                'sticky' => [
+                    'view'      => 'alert-sticky',
+                    'alert'     => Types\Sticky::class,
+                    'component' => Themes\Bootstrap5\Sticky::class,
+                ],
+            ],
         ],
     ],
 ];
