933f6d789e9e3a5ffef434502df43e36abf97270
Emeka Mbah <frankemeks77@yahoo.com>
2024-12-30 20:55:07 +0000

    add themes config

--- config/alert.php
+++ config/alert.php
@@ -27,80 +27,80 @@ return [
     'themes' => [
         'classic' => [
             'components' => 'Digitlimit\Alert\View\Components\Themes\Classic',
-            'types'      => [
+            'types' => [
                 'message' => [
-                    'view'      => 'alert-message',
-                    'alert'     => Types\Message::class,
+                    'view' => 'alert-message',
+                    'alert' => Types\Message::class,
                     'component' => Themes\Classic\Message::class,
                 ],
 
                 'field' => [
-                    'view'      => 'alert-field',
-                    'alert'     => Types\Field::class,
+                    'view' => 'alert-field',
+                    'alert' => Types\Field::class,
                     'component' => Themes\Classic\Field::class,
                 ],
 
                 'bag' => [
-                    'view'      => 'alert-field',
-                    'alert'     => Types\FieldBag::class,
+                    'view' => 'alert-field',
+                    'alert' => Types\FieldBag::class,
                     'component' => Themes\Classic\Field::class,
                 ],
 
                 'modal' => [
-                    'view'      => 'alert-modal',
-                    'alert'     => Types\Modal::class,
+                    'view' => 'alert-modal',
+                    'alert' => Types\Modal::class,
                     'component' => Themes\Classic\Modal::class,
                 ],
 
                 'notify' => [
-                    'view'      => 'alert-notify',
-                    'alert'     => Types\Notify::class,
+                    'view' => 'alert-notify',
+                    'alert' => Types\Notify::class,
                     'component' => Themes\Classic\Notify::class,
                 ],
 
                 'sticky' => [
-                    'view'      => 'alert-sticky',
-                    'alert'     => Types\Sticky::class,
+                    'view' => 'alert-sticky',
+                    'alert' => Types\Sticky::class,
                     'component' => Themes\Classic\Sticky::class,
                 ],
             ],
         ],
         'bootstrap5' => [
             'components' => 'Digitlimit\Alert\View\Components\Themes\Bootstrap5',
-            'types'      => [
+            'types' => [
                 'message' => [
-                    'view'      => 'alert-message',
-                    'alert'     => Types\Message::class,
+                    'view' => 'alert-message',
+                    'alert' => Types\Message::class,
                     'component' => Themes\Bootstrap5\Message::class,
                 ],
 
                 'field' => [
-                    'view'      => 'alert-field',
-                    'alert'     => Types\Field::class,
+                    'view' => 'alert-field',
+                    'alert' => Types\Field::class,
                     'component' => Themes\Bootstrap5\Field::class,
                 ],
 
                 'bag' => [
-                    'view'      => 'alert-field',
-                    'alert'     => Types\FieldBag::class,
+                    'view' => 'alert-field',
+                    'alert' => Types\FieldBag::class,
                     'component' => Themes\Bootstrap5\Field::class,
                 ],
 
                 'modal' => [
-                    'view'      => 'alert-modal',
-                    'alert'     => Types\Modal::class,
+                    'view' => 'alert-modal',
+                    'alert' => Types\Modal::class,
                     'component' => Themes\Bootstrap5\Modal::class,
                 ],
 
                 'notify' => [
-                    'view'      => 'alert-notify',
-                    'alert'     => Types\Notify::class,
+                    'view' => 'alert-notify',
+                    'alert' => Types\Notify::class,
                     'component' => Themes\Bootstrap5\Notify::class,
                 ],
 
                 'sticky' => [
-                    'view'      => 'alert-sticky',
-                    'alert'     => Types\Sticky::class,
+                    'view' => 'alert-sticky',
+                    'alert' => Types\Sticky::class,
                     'component' => Themes\Bootstrap5\Sticky::class,
                 ],
             ],
