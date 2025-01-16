f59c02b9529dd94ca0399a88b7ad6a469a3c2d88
Emeka Mbah <frankemeks77@yahoo.com>
2025-01-16 22:58:31 +0000

    build js

---
+++ resources/js/alert.old.js
@@ -0,0 +1,8 @@
+import '../scss/alert.scss'
+
+export { default as Field } from './components/field.js'
+export { default as Message } from './components/message.js'
+export { default as Modal } from './components/modal.js'
+export { default as Notify } from './components/notify.js'
+export { default as Sticky } from './components/sticky.js'
+
