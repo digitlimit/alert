d37bd575c5287ad437209fc236d2956c2c99faaf
Emeka Mbah <frankemeks77@yahoo.com>
2025-02-05 21:30:38 +0000

    add toaster

--- resources/js/src/toast.js
+++ resources/js/src/toast.js
@@ -1,224 +1,160 @@
-/**
- * --------------------------------------------------------------------------
- * toast.js
- * --------------------------------------------------------------------------
- */
+class Toast
+{
+  constructor() {
+    this.$container = null;
+    this.listener = null;
+    this.toastId = 0;
+    this.previousToast = undefined;
+    this.options = {};
+    this.toastType = {
+      error: 'error',
+      info: 'info',
+      success: 'success',
+      warning: 'warning',
+    };
 
-import BaseComponent from './base-component.js'
-import EventHandler from './dom/event-handler.js'
-import { enableDismissTrigger } from './util/component-functions.js'
-import { defineJQueryPlugin, reflow } from './util/index.js'
-
-/**
- * Constants
- */
-
-const NAME = 'toast'
-const DATA_KEY = 'dl.toast'
-const EVENT_KEY = `.${DATA_KEY}`
-
-const EVENT_MOUSEOVER = `mouseover${EVENT_KEY}`
-const EVENT_MOUSEOUT = `mouseout${EVENT_KEY}`
-const EVENT_FOCUSIN = `focusin${EVENT_KEY}`
-const EVENT_FOCUSOUT = `focusout${EVENT_KEY}`
-const EVENT_HIDE = `hide${EVENT_KEY}`
-const EVENT_HIDDEN = `hidden${EVENT_KEY}`
-const EVENT_SHOW = `show${EVENT_KEY}`
-const EVENT_SHOWN = `shown${EVENT_KEY}`
-
-const CLASS_NAME_FADE = 'fade'
-const CLASS_NAME_HIDE = 'hide' // @deprecated - kept here only for backwards compatibility
-const CLASS_NAME_SHOW = 'show'
-const CLASS_NAME_SHOWING = 'showing'
-
-const DefaultType = {
-  animation: 'boolean',
-  autohide: 'boolean',
-  delay: 'number'
-}
-
-const Default = {
-  animation: true,
-  autohide: true,
-  delay: 5000
-}
-
-/**
- * Class definition
- */
-
-class Toast extends BaseComponent {
-  constructor(element, config) {
-    super(element, config)
-
-    this._timeout = null
-    this._hasMouseInteraction = false
-    this._hasKeyboardInteraction = false
-    this._setListeners()
+    // Initialize any existing toasts on page load
+    this.init();
   }
 
-  // Getters
-  static get Default() {
-    return Default
+  init() {
+    // Check if the toast container exists and has toasts inside
+    this.$container = $('#toastr-container');
+    if (this.$container.length) {
+      this.$container.children('.toastr').each((_, el) => {
+        const $toastElement = $(el);
+        const type = this.getToastType($toastElement);
+        const title = $toastElement.find('.toastr-title').text();
+        const message = $toastElement.find('.toastr-message').text();
+
+        // Rebuild the toast using the existing elements
+        this.notify({
+          type,
+          iconClass: this.getOptions().iconClasses[type],
+          message,
+          title,
+        });
+      });
+    }
   }
 
-  static get DefaultType() {
-    return DefaultType
+  getToastType($toastElement) {
+    if ($toastElement.hasClass('toastr-success')) return 'success';
+    if ($toastElement.hasClass('toastr-error')) return 'error';
+    if ($toastElement.hasClass('toastr-info')) return 'info';
+    if ($toastElement.hasClass('toastr-warning')) return 'warning';
+    return 'info'; // Default type
   }
 
-  static get NAME() {
-    return NAME
+  error(message, title, optionsOverride) {
+    return this.notify({
+      type: this.toastType.error,
+      iconClass: this.getOptions().iconClasses.error,
+      message,
+      optionsOverride,
+      title,
+    });
   }
 
-  // Public
-  show() {
-    const showEvent = EventHandler.trigger(this._element, EVENT_SHOW)
-
-    if (showEvent.defaultPrevented) {
-      return
-    }
-
-    this._clearTimeout()
-
-    if (this._config.animation) {
-      this._element.classList.add(CLASS_NAME_FADE)
-    }
-
-    const complete = () => {
-      this._element.classList.remove(CLASS_NAME_SHOWING)
-      EventHandler.trigger(this._element, EVENT_SHOWN)
-
-      this._maybeScheduleHide()
-    }
-
-    this._element.classList.remove(CLASS_NAME_HIDE) // @deprecated
-    reflow(this._element)
-    this._element.classList.add(CLASS_NAME_SHOW, CLASS_NAME_SHOWING)
-
-    this._queueCallback(complete, this._element, this._config.animation)
+  info(message, title, optionsOverride) {
+    return this.notify({
+      type: this.toastType.info,
+      iconClass: this.getOptions().iconClasses.info,
+      message,
+      optionsOverride,
+      title,
+    });
   }
 
-  hide() {
-    if (!this.isShown()) {
-      return
-    }
-
-    const hideEvent = EventHandler.trigger(this._element, EVENT_HIDE)
-
-    if (hideEvent.defaultPrevented) {
-      return
-    }
-
-    const complete = () => {
-      this._element.classList.add(CLASS_NAME_HIDE) // @deprecated
-      this._element.classList.remove(CLASS_NAME_SHOWING, CLASS_NAME_SHOW)
-      EventHandler.trigger(this._element, EVENT_HIDDEN)
-    }
-
-    this._element.classList.add(CLASS_NAME_SHOWING)
-    this._queueCallback(complete, this._element, this._config.animation)
+  success(message, title, optionsOverride) {
+    return this.notify({
+      type: this.toastType.success,
+      iconClass: this.getOptions().iconClasses.success,
+      message,
+      optionsOverride,
+      title,
+    });
   }
 
-  dispose() {
-    this._clearTimeout()
-
-    if (this.isShown()) {
-      this._element.classList.remove(CLASS_NAME_SHOW)
-    }
-
-    super.dispose()
+  warning(message, title, optionsOverride) {
+    return this.notify({
+      type: this.toastType.warning,
+      iconClass: this.getOptions().iconClasses.warning,
+      message,
+      optionsOverride,
+      title,
+    });
   }
 
-  isShown() {
-    return this._element.classList.contains(CLASS_NAME_SHOW)
-  }
+  notify(map) {
+    let options = this.getOptions();
+    let iconClass = map.iconClass || options.iconClass;
+    if (map.optionsOverride) {
+      options = Object.assign(options, map.optionsOverride);
+      iconClass = map.optionsOverride.iconClass || iconClass;
+    }
+    if (this.shouldExit(options, map)) return;
 
-  // Private
+    this.toastId++;
+    this.$container = this.getContainer(options, true);
 
-  _maybeScheduleHide() {
-    if (!this._config.autohide) {
-      return
+    let $toastElement = $('<div/>')
+        .addClass('toastr')
+        .addClass(iconClass)
+        .append(`<div class="toastr-title">${map.title || ''}</div>`)
+        .append(`<div class="toastr-message">${map.message}</div>`)
+        .append(`<div class="toastr-progress" style="width: 100%;"></div>`);
+
+    if (options.newestOnTop) {
+      this.$container.prepend($toastElement);
+    } else {
+      this.$container.append($toastElement);
     }
 
-    if (this._hasMouseInteraction || this._hasKeyboardInteraction) {
-      return
-    }
-
-    this._timeout = setTimeout(() => {
-      this.hide()
-    }, this._config.delay)
+    setTimeout(() => this.removeToast($toastElement), options.timeOut);
+    return $toastElement;
   }
 
-  _onInteraction(event, isInteracting) {
-    switch (event.type) {
-      case 'mouseover':
-      case 'mouseout': {
-        this._hasMouseInteraction = isInteracting
-        break
-      }
-
-      case 'focusin':
-      case 'focusout': {
-        this._hasKeyboardInteraction = isInteracting
-        break
-      }
-
-      default: {
-        break
-      }
-    }
-
-    if (isInteracting) {
-      this._clearTimeout()
-      return
-    }
-
-    const nextElement = event.relatedTarget
-    if (this._element === nextElement || this._element.contains(nextElement)) {
-      return
-    }
-
-    this._maybeScheduleHide()
+  removeToast($toastElement) {
+    if (!$toastElement) return;
+    $toastElement.fadeOut(500, () => $toastElement.remove());
   }
 
-  _setListeners() {
-    EventHandler.on(this._element, EVENT_MOUSEOVER, event => this._onInteraction(event, true))
-    EventHandler.on(this._element, EVENT_MOUSEOUT, event => this._onInteraction(event, false))
-    EventHandler.on(this._element, EVENT_FOCUSIN, event => this._onInteraction(event, true))
-    EventHandler.on(this._element, EVENT_FOCUSOUT, event => this._onInteraction(event, false))
+  getDefaults() {
+    return {
+      containerId: 'toastr-container',
+      positionClass: 'toastr-bottom-center',
+      iconClasses: {
+        error: 'toastr-error',
+        info: 'toastr-info',
+        success: 'toastr-success',
+        warning: 'toastr-warning',
+      },
+      timeOut: 5000,
+      closeOnHover: true,
+      preventDuplicates: false,
+    };
   }
 
-  _clearTimeout() {
-    clearTimeout(this._timeout)
-    this._timeout = null
+  getOptions() {
+    return Object.assign({}, this.getDefaults(), this.options);
   }
 
-  // Static
-  static jQueryInterface(config) {
-    return this.each(function () {
-      const data = Toast.getOrCreateInstance(this, config)
+  getContainer(options = this.getOptions(), create = false) {
+    this.$container = $('#' + options.containerId);
+    if (!this.$container.length && create) {
+      this.$container = this.createContainer(options);
+    }
+    return this.$container;
+  }
 
-      if (typeof config === 'string') {
-        if (typeof data[config] === 'undefined') {
-          throw new TypeError(`No method named "${config}"`)
-        }
-
-        data[config](this)
-      }
-    })
+  createContainer(options) {
+    this.$container = $('<div/>')
+        .attr('id', options.containerId)
+        .addClass(options.positionClass)
+        .appendTo($(options.target || 'body'));
+    return this.$container;
   }
 }
 
-/**
- * Data API implementation
- */
-
-enableDismissTrigger(Toast)
-
-/**
- * jQuery
- */
-
-defineJQueryPlugin(Toast)
-
-export default Toast
+export default Toast;
