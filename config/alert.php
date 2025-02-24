b7ba26fdc9557da77a733b37e606187412892011
Emeka Mbah <frankemeks77@yahoo.com>
2025-02-24 14:36:13 +0000

    add positions and sizes to config

--- config/alert.php
+++ config/alert.php
@@ -101,12 +101,30 @@ return [
                 'view' => 'alert-modal',
                 'alert' => Types\Modal::class,
                 'component' => Themes\Bootstrap5\Modal::class,
+                'sizes' => [
+                    'default' => 'modal-md',
+                    'small' => 'modal-sm',
+                    'medium' => 'modal-md',
+                    'large' => 'modal-lg',
+                    'extra-large' => 'modal-xl',
+                    'fullscreen' => 'modal-fullscreen',
+                ]
             ],
 
             'notify' => [
                 'view' => 'alert-notify',
                 'alert' => Types\Notify::class,
                 'component' => Themes\Bootstrap5\Notify::class,
+                'positions' => [
+                    'default' => 'top-0 start-0',
+                    'top-right' => 'top-0 end-0',
+                    'top-left' => 'top-0 start-0',
+                    'bottom-right' => 'bottom-0 end-0',
+                    'bottom-left' => 'bottom-0 start-0',
+                    'top-center' => 'top-0 start-50 translate-middle-x',
+                    'bottom-center' => 'bottom-0 start-50 translate-middle-x',
+                    'center' => 'top-50 start-50 translate-middle',
+                ]
             ],
 
             'sticky' => [
