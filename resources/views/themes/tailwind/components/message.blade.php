0d188d1fd7432e9e574b55548742b66873fa598c
Emeka Mbah <frankemeks77@yahoo.com>
2025-03-02 22:24:17 +0000

    fix message title

--- resources/views/themes/tailwind/components/message.blade.php
+++ resources/views/themes/tailwind/components/message.blade.php
@@ -25,7 +25,7 @@
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
         </svg>
 
-        <span class="sr-only">{{ ucwords($message->getLevel()) }}</span>
+        <span class="sr-only-fix">{{ ucwords($message->getLevel()) }}</span>
 
         {{ $slot ?? '' }}
 
@@ -43,7 +43,7 @@
                 class="close {{ $level }}"
                 aria-label="Close"
         >
-            <span class="sr-only">Close</span>
+            <span class="sr-only-fix">Close</span>
             <svg class="close-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path
                         stroke="currentColor"
